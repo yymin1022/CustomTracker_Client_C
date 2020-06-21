17333d73586ee62242349a1091609f4ce601a94b
yymin1022 <yymin1022@gmail.com>
2020-06-22 02:32:46 +0900

    Update Default Menu Layout Code

--- main.c
+++ main.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char *trackNum;
+    char *command = malloc(sizeof(char) * 50);
+    int selMenu;
+
+    strcpy(command, "CustomTracker_Client_Java.exe ");
+
+    printf("Welcome to Custom Tracking Service.\n");
+    printf("Please input your Tracking Number. : ");
+    scanf("%s", trackNum);
+
+    printf("\nPlease select Menu.\n");
+    printf("1. Get Parcel Information\n");
+    printf("2. Get Parcel Tracking Information\n");
+    scanf("%d", &selMenu);
+
+    switch (selMenu){
+        case 1:
+            strcat(command, "Parcel ");
+            break;
+        case 2:
+            strcat(command, "Track ");
+            break;
+    }
+    strcat(command, trackNum);
+
+    system(command);
 
     return 0;
 }
