9c5475b1aa1c7796c798e73556c98e16ef641983
yymin1022 <yymin1022@gmail.com>
2020-06-21 14:18:04 +0900

    Initialized Commit

---
+++ cmake-build-debug/CMakeFiles/CustomTracker_Client_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CustomTracker_Client_C.dir/main.c.obj"
+  "CustomTracker_Client_C.exe"
+  "CustomTracker_Client_C.exe.manifest"
+  "CustomTracker_Client_C.lib"
+  "CustomTracker_Client_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CustomTracker_Client_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
